c71fcc24c9eabaaa08dc0ea9ba0ca7d1f0476baf
sinned <sinned@gmail.com>
2012-05-23 02:14:29 -0700

    added popular and gift urls

--- scrape-urls.php
+++ scrape-urls.php
@@ -13,7 +13,7 @@
 	//$url = "http://localhost/scraperest/dom-test.html";
 	
 	$html = loadContentFromUrl($url);
-	echo $html . "\n\n";
+	//echo $html . "\n\n";
 	
 	// find all of the urls of the form /all?category=SOMETHING
 	
@@ -26,5 +26,25 @@
 			}
 		}
 	}
+	
+	$pattern = '/<a href="(\/popular\/.*?)"/';
+	if (preg_match_all($pattern, $html, $matches)) {
+		foreach ($matches[1] as $url) {
+			$sql = "INSERT INTO pinterest_urls (url) VALUES ('http://pinterest.com" . $db->escape($url) . "')";
+			if ($db->query($sql)) {
+				echo "INSERTED! " . $url . "\n";				
+			}
+		}
+	}	
+	
+	$pattern = '/<a href="(\/gifts\/.*?)"/';
+	if (preg_match_all($pattern, $html, $matches)) {
+		foreach ($matches[1] as $url) {
+			$sql = "INSERT INTO pinterest_urls (url) VALUES ('http://pinterest.com" . $db->escape($url) . "')";
+			if ($db->query($sql)) {
+				echo "INSERTED! " . $url . "\n";				
+			}
+		}
+	}		
 	
 ?>
